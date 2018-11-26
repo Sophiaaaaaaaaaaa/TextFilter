b430b6920b24db581f133222912aa4babf2c5c7a
Sophiaaaaaaaaaaa <33172801+Sophiaaaaaaaaaaa@users.noreply.github.com>
2018-11-26 11:35:18 +0800

    Update 文本处理.py
    这些代码完成的功能是将 提取文本固定行数的数据

--- 文本处理.py
+++ 文本处理.py
@@ -1,4 +1,4 @@
-with open('p.project','r') as reader, open('p.txt', 'w') as writer:
+with open('v.project','r') as reader, open('v.txt', 'w') as writer:
     for index, line in enumerate(reader):
         if index % 12 == 9:
             line=line.strip('\n')
